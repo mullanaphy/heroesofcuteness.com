288c7d875babe7444015c12e3314593abd99f619
John Mullanaphy <hi@john.mu>
2024-11-18 11:50:44 -0500

    Clean up the listener a bit

--- src/Listener/SearchListener.php
+++ src/Listener/SearchListener.php
@@ -14,13 +14,15 @@ use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\Event\PostFlushEventArgs;
 use Doctrine\ORM\Events;
 use Doctrine\ORM\Query\ResultSetMapping;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Michelf\Markdown;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 #[AsDoctrineListener(event: Events::onFlush)]
 #[AsDoctrineListener(event: Events::postFlush)]
-class DoctrineOnFlushListener
+#[AsDoctrineListener(event: Events::preRemove)]
+class SearchListener
 {
     private const INSERT = 'INSERT INTO search (entity, entity_id, content, created, updated) VALUES (:entity, :entityId, :content, :created, :updated)';
     private const UPDATE = 'UPDATE search SET content = :content WHERE entity = :entity AND entity_id = :entityId';
@@ -36,6 +38,13 @@ class DoctrineOnFlushListener
 
     }
 
+    /**
+     * We're going to first collect any entity changes that would involve search. Once stored we'll then process them
+     * accordingly in postFlush.
+     *
+     * @param OnFlushEventArgs $eventArgs
+     * @return void
+     */
     public function onFlush(OnFlushEventArgs $eventArgs): void
     {
         $manager = $eventArgs->getObjectManager();
@@ -52,20 +61,61 @@ class DoctrineOnFlushListener
         }
     }
 
-    private static function cleanContent(array $content): string
+    /**
+     * Now that we have search data to save via postFlush, we'll now save them accordingly.
+     *
+     * @param PostFlushEventArgs $eventArgs
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    public function postFlush(PostFlushEventArgs $eventArgs): void
     {
-        return strtolower(
-            preg_replace('/[^A-Za-z0-9 ]/', '',
-                preg_replace('/[\t\r\n ]+/', ' ',
-                    strip_tags(
-                        implode(' ', array_filter($content, function ($item) {
-                                return is_string($item) && $item;
-                            })
-                        )
-                    )
-                )
-            )
-        );
+        if (!count($this->toSave)) {
+            return;
+        }
+
+        foreach ($this->toSave as $pair) {
+            /* @var Search $search */
+            /* @var Comic|Character $entity */
+            list($search, $entity) = $pair;
+
+            $this->entityManager->createNativeQuery($search->getEntityId() ? self::UPDATE : self::INSERT, new ResultSetMapping())
+                ->setParameters([
+                    'entity' => $search->getEntity(),
+                    'entityId' => $entity->getId(),
+                    'content' => $search->getContent(),
+                    'created' => $search->getCreated(),
+                    'updated' => $search->getUpdated(),
+                ])
+                ->execute();
+        }
+
+        $this->toSave = [];
+        $this->cache->invalidateTags(['comics']);
+    }
+
+    /**
+     * If an entity is removed that would have search, we'll need to remove that now as well.
+     *
+     * @param LifecycleEventArgs $eventArgs
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    public function preRemove(LifecycleEventArgs $eventArgs): void
+    {
+        $entity = $eventArgs->getObject();
+
+        if ($entity instanceof Comic) {
+            $this->searchRepository->remove('Comic', $entity->getId());
+        } else if ($entity instanceof Panel || $entity instanceof Hidden) {
+            $this->searchRepository->remove('Comic', $entity->getComic()->getId());
+        } else if ($entity instanceof Character) {
+            $this->searchRepository->remove('Character', $entity->getId());
+        } else {
+            return;
+        }
+
+        $this->cache->invalidateTags(['comics']);
     }
 
     private function pairSearchAndEntity(Character|Comic $entity): array
@@ -87,33 +137,6 @@ class DoctrineOnFlushListener
         ));
 
         return [$search, $entity];
-    }
-
-    /**
-     * @throws InvalidArgumentException
-     */
-    public function postFlush(PostFlushEventArgs $eventArgs): void
-    {
-        foreach ($this->toSave as $pair) {
-            /* @var Search $search */
-            /* @var Comic|Character $entity */
-            list($search, $entity) = $pair;
-
-            $this->entityManager->createNativeQuery($search->getEntityId() ? self::UPDATE : self::INSERT, new ResultSetMapping())
-                ->setParameters([
-                    'entity' => $search->getEntity(),
-                    'entityId' => $entity->getId(),
-                    'content' => $search->getContent(),
-                    'created' => $search->getCreated(),
-                    'updated' => $search->getUpdated(),
-                ])
-                ->execute();
-        }
-
-        if (count($this->toSave)) {
-            $this->toSave = [];
-            $this->cache->invalidateTags(['comics']);
-        }
     }
 
     private function getComicContent(Comic $comic): array
@@ -157,5 +180,21 @@ class DoctrineOnFlushListener
         $content[] = $biography;
 
         return $content;
+    }
+
+    private static function cleanContent(array $content): string
+    {
+        return strtolower(
+            preg_replace('/[^A-Za-z0-9 ]/', '',
+                preg_replace('/[\t\r\n ]+/', ' ',
+                    strip_tags(
+                        implode(' ', array_filter($content, function ($item) {
+                                return is_string($item) && $item;
+                            })
+                        )
+                    )
+                )
+            )
+        );
     }
 }
